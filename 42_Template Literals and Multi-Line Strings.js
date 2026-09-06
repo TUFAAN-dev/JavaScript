9215254de92efb88ac04924bcb0b65c717d8f89c
TUFAAN-dev <legendarydanish786@gmail.com>
2026-09-06 23:55:39 +0500

    JavaScript Challenge

---
+++ 42_Template Literals and Multi-Line Strings.js
@@ -0,0 +1,10 @@
+const name = "Danish";
+const greeting = `Hello ${name},
+welcome to our site`;
+console.log(greeting);
+
+// Multi-Line without escape
+const multiLine = `Line 1
+Line 2`;
+console.log(multiLine);
+
---
+++ 43_'trim', 'padStart', 'padEnd'.js
@@ -0,0 +1,7 @@
+const str = "  Hello   ";
+console.log(str.trim());            // "Hello"
+console.log(str.trimStart());       // "Hello   "
+console.log(str.trimEnd());         // "  Hello"
+console.log("5".padStart(3, "0"));  // "005"
+console.log("5".padEnd(3, "0"));    // "500"
+
